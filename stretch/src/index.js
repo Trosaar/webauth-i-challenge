48129f9aa0d9cc6f7f2eb098e11393c17203b9b4
Aaron T <troskaaaron@gmail.com>
2019-09-07 13:36:08 -0500

    updated react packages

--- stretch/src/index.js
+++ stretch/src/index.js
@@ -2,5 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+	rootReducer,
+	applyMiddleware(thunk, logger)
+);
+
+ReactDOM.render(
+	<Provider store = {store}>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</Provider>,
+document.getElementById('root'));
